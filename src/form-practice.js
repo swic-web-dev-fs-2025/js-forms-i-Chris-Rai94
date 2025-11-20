539b420a6e4df6904eb59a29ff8b55c60ab5f00c
Christopher Stokley <christopher.stokley@student.swic.edu>
2025-11-20 09:07:34 -0600

    Refactor form submission handling to display results dynamically

--- src/form-practice.js
+++ src/form-practice.js
@@ -1,4 +1,7 @@
+import "./style.css";
+
 const form = document.querySelector("form");
+const result = document.querySelector("#result");
 
 // Same pattern as 'clicks', but for 'submit' events.
 form.addEventListener("submit", (event) => {
@@ -6,6 +9,16 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const formData = new FormData(form);
   const data = Object.fromEntries(formData);
-
-  console.info(data);
+  result.innerHTML = Results(data);
 });
+
+function Results(data) {
+  console.info(Object.entries(data));
+
+  return `<ul class="space-y-1 text-sm">
+        ${Object.entries(data)
+          .map(([k, v]) => `<li><strong>${k}:</strong> ${v}</li>`)
+          .join("")}
+    </ul>   
+    `;
+}
