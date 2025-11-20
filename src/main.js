683e0e1a1c270343c795924c1d831b55c63c4077
Christopher Stokley <christopher.stokley@student.swic.edu>
2025-11-20 13:52:36 -0600

    Implement dynamic submit button enabling based on form input validation

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
-  <h1 class="text-3xl font-bold underline">
-    Hello Vite!
-  </h1>
-`;
+const form = document.querySelector("form");
+const formData = new FormData(form);
+const data = Object.fromEntries(formData);
+const submitBtn = document.querySelector('[type="submit"]');
+const REQUIRED = ["username", "password", "email", "confirmPassword"];
+
+submitBtn.disabled = true;
+REQUIRED.every((field) => form[field].value.trim() !== "");
+form.addEventListener("input", () => {
+  // Does EVERY form input field have a non-empty value?
+  REQUIRED.every((field) => form[field].value.trim() !== "")
+    ? (submitBtn.disabled = false) // If yes, enable button
+    : (submitBtn.disabled = true); // If no, disable button
+});
