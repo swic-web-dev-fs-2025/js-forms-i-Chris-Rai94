db449c2ee17bddfe88a8b871126c82ac00baf32e
Christopher Stokley <christopher.stokley@student.swic.edu>
2025-11-20 14:48:20 -0600

    Refactor form input handling and remove unnecessary FormData initialization

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import "./style.css";
 
 const form = document.querySelector("form");
-const formData = new FormData(form);
-const data = Object.fromEntries(formData);
-const submitBtn = document.querySelector('[type="submit"]');
+const submitBtn = form.querySelector('[type="submit"]');
 const output = document.getElementsByTagName("output")[0];
 const REQUIRED = ["username", "password", "email", "confirmPassword"];
 
 submitBtn.disabled = true;
-REQUIRED.every((field) => form[field].value.trim() !== "");
 form.addEventListener("input", () => {
   // Does EVERY form input field have a non-empty value?
   REQUIRED.every((field) => form[field].value.trim() !== "")
