3b434386e90be0caae44f3ef91ab81644cb6a95b
Christopher Stokley <christopher.stokley@student.swic.edu>
2025-11-20 14:00:18 -0600

    Add form submission validation for password matching

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ const form = document.querySelector("form");
 const formData = new FormData(form);
 const data = Object.fromEntries(formData);
 const submitBtn = document.querySelector('[type="submit"]');
+const output = document.getElementsByTagName("output")[0];
 const REQUIRED = ["username", "password", "email", "confirmPassword"];
 
 submitBtn.disabled = true;
@@ -13,4 +14,13 @@ form.addEventListener("input", () => {
   REQUIRED.every((field) => form[field].value.trim() !== "")
     ? (submitBtn.disabled = false) // If yes, enable button
     : (submitBtn.disabled = true); // If no, disable button
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (form.password.value !== form.confirmPassword.value) {
+    output.value = "Error: Passwords do not match.";
+    return;
+  }
+  output.value = "";
 });
